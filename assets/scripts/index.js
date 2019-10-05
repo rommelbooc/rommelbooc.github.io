7003e4f3e6fa668fdc26acc315c6b0d0b1e92267
Rommel Booc <boocrommel55@gmail.com>
2019-10-05 23:17:37 +0800

    update

---
+++ assets/scripts/index.js
@@ -0,0 +1,44 @@
+const navSlide = () => {
+  const burger = document.querySelector( '.burger' );
+  const nav = document.querySelector( '.nav-links' );
+  const navLinks = document.querySelectorAll( '.nav-links li' );
+
+  burger.addEventListener( 'click', () => {
+    nav.classList.toggle( 'nav-active' );
+    console.log( 'sdfsd' );
+    navLinks.forEach( ( link, index ) => {
+      if ( link.style.animation ) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+      }
+    } );
+
+    burger.classList.toggle( 'toggle' );
+  } );
+}
+
+$( document ).ready( function () {
+
+  $( "a" ).on( 'click', function ( event ) {
+
+
+    if ( this.hash !== "" ) {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $( 'html, body' ).animate( {
+        scrollTop: $( hash ).offset().top
+      }, 800, function () {
+
+
+        window.location.hash = hash;
+      } );
+    }
+  } );
+} );
+
+navSlide();
+
+AOS.init();

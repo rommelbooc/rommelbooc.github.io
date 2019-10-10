a43a2bbe5b6826602738992c231283c30d0518ff
Rommel Booc <boocrommel55@gmail.com>
2019-10-10 23:50:55 +0800

    update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,19 @@ const navSlide = () => {
 
   burger.addEventListener( 'click', () => {
     nav.classList.toggle( 'nav-active' );
-    console.log( 'sdfsd' );
+    navLinks.forEach( ( link, index ) => {
+      if ( link.style.animation ) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+      }
+    } );
+
+    burger.classList.toggle( 'toggle' );
+  } );
+
+  nav.addEventListener( 'click', () => {
+    nav.classList.toggle( 'nav-active' );
     navLinks.forEach( ( link, index ) => {
       if ( link.style.animation ) {
         link.style.animation = '';
